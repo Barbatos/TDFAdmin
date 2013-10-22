640d7b872a32317651189fd009efe0b6321b1b5a
Barbatos <barbatos@urbanterror.info>
2013-10-22 10:18:34 +0200

    Removed temp message

--- modules/index/index.php
+++ modules/index/index.php
@@ -7,7 +7,5 @@ include_once(BASEPATH.'/modules/header.php');
 
 <h1>Accueil</h1>
 <br />
-<br />
-<p style="text-align:center;font-size:9em;color:pink;font-weight:bold;margin-bottom:40px;text-transform:uppercase;">#Bonjour</p>
 
 <?php include_once(BASEPATH.'/modules/footer.php'); ?>
