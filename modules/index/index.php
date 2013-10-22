399c95ce37f96948eda97aef0db6cddf8281ae80
Barbatos <barbatos@urbanterror.info>
2013-10-22 23:51:52 +0200

    Connection page on the index

--- modules/index/index.php
+++ modules/index/index.php
@@ -8,4 +8,47 @@ include_once(BASEPATH.'/modules/header.php');
 <h1>Accueil</h1>
 <br />
 
-<?php include_once(BASEPATH.'/modules/footer.php'); ?>
+<?php 
+if(!$admin->isLogged()){
+
+	if(P()){
+		$admin->login();
+	}
+?>
+
+<p>Bienvenue sur l'interface d'administration du Tour de France. Merci de vous connecter !</p>
+
+<br /><br />
+
+<form name="connexion" method="post" action="" class="form-horizontal">
+	<div class="control-group">
+		<label class="control-label" for="pseudo">Pseudo</label>
+		<div class="controls">
+			<input type="text" name="pseudo" placeholder="Pseudonyme" />
+		</div>
+	</div>
+
+	<div class="control-group">
+		<label class="control-label" for="pseudo">Password</label>
+		<div class="controls">
+			<input type="password" name="password" placeholder="Mot de Passe" />
+		</div>
+	</div>
+
+	<div class="control-group">
+		<div class="controls">
+			<input type="submit" name="submit" class="btn" value="Connexion" />
+		</div>
+	</div>
+</form>
+
+<?php 
+}
+else {
+?>
+
+<p>Hey <strong><?= $admin->getPseudo() ?></strong> ! Bienvenue sur l'interface d'administration du Tour de France.</p>
+<?php 
+}
+
+include_once(BASEPATH.'/modules/footer.php'); 
